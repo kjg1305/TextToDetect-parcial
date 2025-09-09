2f7836eea8c90cc2b9cb3d77cba2ceaf670b8d78
kjg1305 <kjgonzalezp@academia.usbbog.edu.co>
2025-09-09 18:25:51 +0000

    texto frances cambio

--- KAREN JUDITH GONZALEZ PEÑA/TextoFrances.py
+++ KAREN JUDITH GONZALEZ PEÑA/TextoFrances.py
@@ -2,14 +2,14 @@ import re
 #with open('espanol.txt', 'r', encoding='latin-1') as f:
    # texto_espanol = f.read()
 
-texto_espanol =" Salut! En 2025, 22 designers imaginent ensemble. Liste: crayon, papier, règle. Le prix est de 93,40€. Les étoiles (★) brillent la nuit. 18 chats dessinent, 17 chiens créent. Le code #5566 est spécial. 21 jours de création, 15 jours de repos. @tous imaginent. Le numéro magique est 1424. Que feriez-vous avec 63,70€? La réponse est dans la liste: dessiner, créer, innover. Imaginez votre monde! 100 mots, 21 entiers, 3 decimales, 2 listas."
+texto_espanol ="Salut! En 2025, 22 designers imaginent ensemble. Liste: crayon, papier, règle. Le prix est de 93,40€. Les étoiles (★) brillent la nuit. 18 chats dessinent, 17 chiens créent. Le code #5566 est spécial. 21 jours de création, 15 jours de repos. @tous imaginent. Le numéro magique est 1424. Que feriez-vous avec 63,70€? La réponse est dans la liste: dessiner, créer, innover. Imaginez votre monde! 100 mots, 21 entiers, 3 decimales, 2 listas."
 # se crean las variables correspondientes para cada tipo de busqueda y se les asigna el comando para encontar los respectivos tipos de datos 
 enteros = r"\b-?\d+\b"
 flotantes= r"-?\b\d+\,\d+\b"
 booleanos = r"\b(True|False)\b"
 strings = r'"(.*?)"'
 listas =  r"(?i)liste:\s*([^.\n]+)"
-palabras =  r"\b[a-zA-ZáéíóúÁÉÍÓÚñÑüÜ]+\b"
+palabras =   r"\b[a-zA-ZáéíóúÁÉÍÓÚàèìòùÀÈÌÒÙñÑüÜ]+\b"
 
 
 buscar_enteros = re.findall(enteros,texto_espanol) 
